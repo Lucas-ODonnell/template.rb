7c6f2e5b3469e8ddeb896aff11d5b5c7cfb379c5
Lucas <lucas.odonnelll@gmail.com>
2021-03-10 13:09:44 -0800

    template for gems I want to preload

---
+++ template.rb
@@ -0,0 +1,9 @@
+gem 'devise', '~> 4.7', '>= 4.7.3'
+
+gem_group :development do
+  gem 'better_errors', '~> 2.9', '>= 2.9.1'
+  gem 'guard', '~> 2.16', '>= 2.16.2'
+  gem 'guard-livereload', '~> 2.5', '>= 2.5.2'
+  gem 'pry-rails', '~> 0.3.9'
+  gem 'binding_of_caller', '~> 1.0'
+end
